ac47cc5697160b4934778afd260b6577e420cd3c
David Garden <dawidgrden.99@gmail.com>
2023-09-02 23:14:55 +0200

    doing stuff

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+//    _    _ _    _ _____ _____
+//   | |  | | |  | |_   _|  __ \
+//   | |  | | |  | | | | | |  | |
+//   | |  | | |  | | | | | |  | |
+//   | |__| | |__| |_| |_| |__| |
+//    \____/ \____/|_____|_____/
+//
+//
+
 //https://developer.mozilla.org/en-US/docs/Web/API/Crypto/getRandomValues
 function getRandomNumbers() {
   const typedArray = new Uint8Array(10);
@@ -5,23 +14,32 @@ function getRandomNumbers() {
   return randomValues.join("");
 }
 
+//     _____ _                _____ _____ ______  _____
+//    / ____| |        /\    / ____/ ____|  ____|/ ____|
+//   | |    | |       /  \  | (___| (___ | |__  | (___
+//   | |    | |      / /\ \  \___ \\___ \|  __|  \___ \
+//   | |____| |____ / ____ \ ____) |___) | |____ ____) |
+//    \_____|______/_/    \_\_____/_____/|______|_____/
+//
+//
+
 class Note {
   constructor(
     id = null,
     title = "Unknown",
     content = "Nothing",
     dateOfCreation = null,
-    dateOfModification = null,
+    dateOfModification = null
   ) {
     this.id = id;
     this.title = title;
     this.content = content;
     this.dateOfCreation = dateOfCreation;
-    this.dateOfModification = dateOfModification
+    this.dateOfModification = dateOfModification;
   }
 
-  initialise(){
-    this.id = getRandomNumbers()
+  initialise() {
+    this.id = getRandomNumbers();
     this.dateOfCreation = new Date();
     this.dateOfModification = new Date();
   }
@@ -48,7 +66,7 @@ class NotesLibrary {
     const noteToUpdate = this.getNote(id);
 
     if (noteToUpdate) {
-      noteToUpdate.title = title
+      noteToUpdate.title = title;
       noteToUpdate.content = content;
       noteToUpdate.dateOfModification = new Date();
     } else {
@@ -57,35 +75,48 @@ class NotesLibrary {
   }
 }
 
+//    _____ _   _ _____ _______   ______ _      ______ __  __ ______ _   _ _______ _____
+//   |_   _| \ | |_   _|__   __| |  ____| |    |  ____|  \/  |  ____| \ | |__   __/ ____|
+//     | | |  \| | | |    | |    | |__  | |    | |__  | \  / | |__  |  \| |  | | | (___
+//     | | | . ` | | |    | |    |  __| | |    |  __| | |\/| |  __| | . ` |  | |  \___ \
+//    _| |_| |\  |_| |_   | |    | |____| |____| |____| |  | | |____| |\  |  | |  ____) |
+//   |_____|_| \_|_____|  |_|    |______|______|______|_|  |_|______|_| \_|  |_| |_____/
+//
+//
+
+var supportsTouch = 'ontouchstart' in window || navigator.msMaxTouchPoints;
+
 let currentNoteId = 0;
 let currentNoteIdMultiselection = [];
 
-const notesLibrary = new NotesLibrary()
+const notesLibrary = new NotesLibrary();
 
 const addNoteBtn = document.getElementById("addNoteBtn");
+const delNoteBtn = document.getElementById("delNoteBtn");
 const overlay = document.getElementById("overlay");
 const showNoteModal = document.getElementById("noteModal");
 const noteModalTitle = document.getElementById("title");
 const noteModalContent = document.getElementById("content");
-const notesGrid = document.getElementById('notesGrid')
-const dropdown = document.getElementById('dropdown')
-
+const notesGrid = document.getElementById("notesGrid");
+const dropdown = document.getElementById("dropdown");
 
 const multiSelectionMode = {
   value: false,
   set(newValue) {
     this.value = newValue;
-    const event = new CustomEvent('multiselectionStateChange', { detail: newValue });
+    const event = new CustomEvent("multiselectionStateChange", {
+      detail: newValue,
+    });
     document.dispatchEvent(event);
   },
   get() {
     return this.value;
-  }
+  },
 };
 
 function clearNoteModalFields() {
-  noteModalTitle.value = '';
-  noteModalContent.value = '';
+  noteModalTitle.value = "";
+  noteModalContent.value = "";
 }
 
 function loadNoteModalValues(id) {
@@ -95,8 +126,8 @@ function loadNoteModalValues(id) {
 }
 
 const openNoteModalForm = (id) => {
-  clearNoteModalFields()
-  loadNoteModalValues(id)
+  clearNoteModalFields();
+  loadNoteModalValues(id);
   showNoteModal.classList.add("active");
   overlay.classList.add("active");
 };
@@ -110,22 +141,26 @@ const closeAllModals = () => {
 
 overlay.onclick = closeAllModals;
 
-
 const addNote = () => {
-  const currentNote = new Note()
-  currentNote.initialise()
+  const currentNote = new Note();
+  currentNote.initialise();
 
-  currentNoteId = currentNote.id
-  notesLibrary.addNote(currentNote)
+  currentNoteId = currentNote.id;
+  notesLibrary.addNote(currentNote);
 
   openNoteModalForm(currentNoteId);
-  saveToLocalStorage(currentNoteId)
+  saveToLocalStorage(currentNoteId);
 };
 addNoteBtn.onclick = addNote;
 
-
-
-//Local Storage
+//    _      ____   _____          _           _____ _______ ____  _____            _____ ______
+//   | |    / __ \ / ____|   /\   | |         / ____|__   __/ __ \|  __ \     /\   / ____|  ____|
+//   | |   | |  | | |       /  \  | |        | (___    | | | |  | | |__) |   /  \ | |  __| |__
+//   | |   | |  | | |      / /\ \ | |         \___ \   | | | |  | |  _  /   / /\ \| | |_ |  __|
+//   | |___| |__| | |____ / ____ \| |____     ____) |  | | | |__| | | \ \  / ____ \ |__| | |____
+//   |______\____/ \_____/_/    \_\______|   |_____/   |_|  \____/|_|  \_\/_/    \_\_____|______|
+//
+//
 
 //Official Func form: https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API
 function storageAvailable(type) {
@@ -163,15 +198,22 @@ if (storageAvailable("localStorage")) {
   console.log("err");
 }
 
-
 // Local Storage
 const titleInput = document.getElementById("title");
 const contentInput = document.getElementById("content");
 
 const saveLocal = () => {
-  localStorage.setItem('library', JSON.stringify(notesLibrary.notes))
-}
+  localStorage.setItem("library", JSON.stringify(notesLibrary.notes));
+};
 
+//    _____  ______ ____   ____  _    _ _   _  _____ ______     _____ _   _ _____  _    _ _______
+//   |  __ \|  ____|  _ \ / __ \| |  | | \ | |/ ____|  ____|   |_   _| \ | |  __ \| |  | |__   __|
+//   | |  | | |__  | |_) | |  | | |  | |  \| | |    | |__        | | |  \| | |__) | |  | |  | |
+//   | |  | |  __| |  _ <| |  | | |  | | . ` | |    |  __|       | | | . ` |  ___/| |  | |  | |
+//   | |__| | |____| |_) | |__| | |__| | |\  | |____| |____     _| |_| |\  | |    | |__| |  | |
+//   |_____/|______|____/ \____/ \____/|_| \_|\_____|______|   |_____|_| \_|_|     \____/   |_|
+//
+//
 
 // Debouncing function to delay function execution
 function debounce(func, delay) {
@@ -188,85 +230,138 @@ function saveToLocalStorage(id) {
   const title = titleInput.value;
   const content = contentInput.value;
 
-  notesLibrary.updateNoteContent(id, title, content)
+  notesLibrary.updateNoteContent(id, title, content);
 
   saveLocal();
   renderNotesLibrary();
 }
 
 // Debounced version of the saveToLocalStorage function
-const debouncedSaveToLocalStorage = debounce((id) => saveToLocalStorage(id), 500);
+const debouncedSaveToLocalStorage = debounce(
+  (id) => saveToLocalStorage(id),
+  500
+);
 
 // Add event listeners to input fields
-titleInput.addEventListener("input", () => debouncedSaveToLocalStorage(currentNoteId));
-contentInput.addEventListener("input", () => debouncedSaveToLocalStorage(currentNoteId));
+titleInput.addEventListener("input", () =>
+  debouncedSaveToLocalStorage(currentNoteId)
+);
+contentInput.addEventListener("input", () =>
+  debouncedSaveToLocalStorage(currentNoteId)
+);
 
 const JSONToNote = (note) => {
-  return new Note(note.id, note.title, note.content, note.dateOfCreation, note.dateOfModification)
-}
+  return new Note(
+    note.id,
+    note.title,
+    note.content,
+    note.dateOfCreation,
+    note.dateOfModification
+  );
+};
 
 function removeSelectedClassNoteCard() {
-  currentNoteId = null
-  const childElements = notesGrid.querySelectorAll('.note-card');
-  childElements.forEach(child => {
-    child.classList.remove('selected');
+  currentNoteId = null;
+  addNoteBtn.classList.add("active");
+  delNoteBtn.classList.remove("active");
+  const childElements = notesGrid.querySelectorAll(".note-card");
+  childElements.forEach((child) => {
+    child.classList.remove("selected");
   });
 }
 
-
-document.addEventListener('multiselectionStateChange', (event) => {
+document.addEventListener("multiselectionStateChange", (event) => {
   if (event.detail === true) {
-    dropdown.classList.add('selected');
-  }else{
-    dropdown.classList.remove('selected');
+    dropdown.classList.add("selected");
+    addNoteBtn.classList.remove("active");
+    delNoteBtn.classList.add("active");
+  } else {
+    dropdown.classList.remove("selected");
+    addNoteBtn.classList.add("active");
+    delNoteBtn.classList.remove("active");
   }
 });
 
+//    _   _  ____ _______ ______      _____          _____  _____
+//   | \ | |/ __ \__   __|  ____|    / ____|   /\   |  __ \|  __ \
+//   |  \| | |  | | | |  | |__      | |       /  \  | |__) | |  | |
+//   | . ` | |  | | | |  |  __|     | |      / /\ \ |  _  /| |  | |
+//   | |\  | |__| | | |  | |____    | |____ / ____ \| | \ \| |__| |
+//   |_| \_|\____/  |_|  |______|    \_____/_/    \_\_|  \_\_____/
+//
+//
+
 
 const createNoteCard = (note) => {
-  const noteCard = document.createElement('div');
-  const title = document.createElement('p');
-  const content = document.createElement('p');
+  const noteCard = document.createElement("div");
+  const title = document.createElement("p");
+  const content = document.createElement("p");
 
-  noteCard.classList.add('note-card');
-  noteCard.setAttribute('data-card-id', note.id);
+  noteCard.classList.add("note-card");
+  noteCard.setAttribute("data-card-id", note.id);
 
-  title.classList.add('card-title');
-  content.classList.add('card-content');
+  title.classList.add("card-title");
+  content.classList.add("card-content");
 
-  title.textContent = `${note.title}`
-  content.textContent = `${note.content}`
+  title.textContent = `${note.title}`;
+  content.textContent = `${note.content}`;
 
   let isMousePressed = false;
   let mousePressTimer;
 
-  noteCard.addEventListener('mousedown', () => {
-    mousePressTimer = setTimeout(() => {
+  const touchCard = new Touch({
+    identifier: "123",
+    target: noteCard,
+  });
+
+  const touchEventStart = new TouchEvent("touchstart", {
+    touches: [touchCard],
+    view: window,
+    cancelable: true,
+    bubbles: true,
+  });
+
+  const touchEventEnd = new TouchEvent("touchend", {
+    touches: [],
+    view: null,
+    cancelable: true,
+    bubbles: true,
+  });
+  
+  noteCard.dispatchEvent(touchEventStart);
+  noteCard.dispatchEvent(touchEventEnd);
+
+
+  function startPress() {
+    if (multiSelectionMode.get() === false){
+       mousePressTimer = setTimeout(() => {
       multiSelectionMode.set(true);
     }, 1000);
-    isMousePressed = true
-  });
-  noteCard.addEventListener('mouseup', () => {
+    }
+   
+    isMousePressed = true;
+  }
+
+  function endPress() {
     if (isMousePressed) {
       // Clear the timer
       clearTimeout(mousePressTimer);
-
       // If in single-selection mode, select the clicked card
       if (multiSelectionMode.get() === false) {
         removeSelectedClassNoteCard();
-        noteCard.classList.add('selected');
-        currentNoteId = noteCard.getAttribute('data-card-id');
+        noteCard.classList.add("selected");
+        currentNoteId = noteCard.getAttribute("data-card-id");
         openNoteModalForm(currentNoteId);
       }
 
       // If in multi-selection mode, toggle the card's selection state
       if (multiSelectionMode.get() === true) {
-        noteCard.classList.toggle('selected');
-        const clickNoteId = noteCard.getAttribute('data-card-id');
-        if (noteCard.classList.contains('selected')) {
+        noteCard.classList.toggle("selected");
+        const clickNoteId = noteCard.getAttribute("data-card-id");
+        if (noteCard.classList.contains("selected")) {
           currentNoteIdMultiselection.push(clickNoteId);
         } else {
-          //Select indexOf clickedNoteId and delete from array by index 
+          //Select indexOf clickedNoteId and delete from array by index
           const index = currentNoteIdMultiselection.indexOf(clickNoteId);
           if (index !== -1) {
             currentNoteIdMultiselection.splice(index, 1);
@@ -275,51 +370,91 @@ const createNoteCard = (note) => {
       }
       isMousePressed = false;
     }
+  }
+
+  if (supportsTouch) {
+      noteCard.addEventListener("touchstart", (e) =>{
+        e.preventDefault()
+    startPress();
+  })
+  noteCard.addEventListener("touchend", (e) =>{
+    e.preventDefault()
+    endPress();
+  })
+  }else{
+     noteCard.addEventListener("mousedown", () => {
+    startPress();
   });
+  noteCard.addEventListener("mouseup", () => {
+    endPress();
+  });
+  }
+
+
+ 
+
 
   noteCard.appendChild(title);
   noteCard.appendChild(content);
 
-  notesGrid.appendChild(noteCard)
-}
+  notesGrid.appendChild(noteCard);
+};
 
 const resetNotesGrid = () => {
-  notesGrid.innerHTML = ''
-}
+  notesGrid.innerHTML = "";
+};
 
 function renderNotesLibrary() {
   resetNotesGrid();
-  notesLibrary.notes.forEach(note => {
-    createNoteCard(note)
+  notesLibrary.notes.forEach((note) => {
+    createNoteCard(note);
   });
 }
 
+//    _____        _____ ______     _      ____          _____
+//   |  __ \ /\   / ____|  ____|   | |    / __ \   /\   |  __ \
+//   | |__) /  \ | |  __| |__      | |   | |  | | /  \  | |  | |
+//   |  ___/ /\ \| | |_ |  __|     | |   | |  | |/ /\ \ | |  | |
+//   | |  / ____ \ |__| | |____    | |___| |__| / ____ \| |__| |
+//   |_| /_/    \_\_____|______|   |______\____/_/    \_\_____/
+//
+//
 
-
-// Load data from local storage on page load
 window.addEventListener("load", () => {
   const savedNotesLibrary = JSON.parse(localStorage.getItem("library"));
   if (savedNotesLibrary) {
-    notesLibrary.notes = savedNotesLibrary.map((note) => JSONToNote(note))
+    notesLibrary.notes = savedNotesLibrary.map((note) => JSONToNote(note));
     renderNotesLibrary();
-    console.log(notesLibrary)
-  }else{
+    console.log(notesLibrary);
+  } else {
     notesLibrary.notes = [];
   }
 });
 
+//    _____  ______  _____ ______ _      ______ _____ _______     ________      _________ _      _____  _____ _______
+//   |  __ \|  ____|/ ____|  ____| |    |  ____/ ____|__   __|   |  ____\ \    / /__   __| |    |_   _|/ ____|__   __|
+//   | |  | | |__  | (___ | |__  | |    | |__ | |       | |      | |__   \ \  / /   | |  | |      | | | (___    | |
+//   | |  | |  __|  \___ \|  __| | |    |  __|| |       | |      |  __|   \ \/ /    | |  | |      | |  \___ \   | |
+//   | |__| | |____ ____) | |____| |____| |___| |____   | |      | |____   \  /     | |  | |____ _| |_ ____) |  | |
+//   |_____/|______|_____/|______|______|______\_____|  |_|      |______|   \/      |_|  |______|_____|_____/   |_|
+//
+//
 
-// Deselect all 
-document.addEventListener("click", function(event) {
+document.addEventListener("click", function (event) {
   var clickedElement = event.target;
   // Check if the clicked element or any of its ancestors have the "card" class
   var isClickedOnCard = clickedElement.closest(".note-card");
-  var isClickedOnModalInput = clickedElement.closest(".note-form")
-
-  console.log(isClickedOnCard)
+  var isClickedOnModalInput = clickedElement.closest(".note-form");
+  var clickedOnButtons = clickedElement.closest(".btn");
+  console.log(isClickedOnCard);
   // If not clicked on a card or its descendant
-  if (!isClickedOnCard && !isClickedOnModalInput) {
+  if (!isClickedOnCard && !isClickedOnModalInput && !clickedOnButtons) {
     multiSelectionMode.set(false);
     removeSelectedClassNoteCard();
   }
+});
+
+
+window.addEventListener("contextmenu", (e) => {
+  e.preventDefault();
 });
